9f187d2bb6f2c3c98f3e3b9912c7225a190298d9
hartphoenix <161499832+hartphoenix@users.noreply.github.com>
2025-12-14 23:46:37 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const inputEl = document.getElementById("input-el")
+const btnEl = document.getElementById("btn-el")
+const outputEl = document.getElementById("output-el")
+
+function primeFactorsOf(x, y = [], z = 2) {
+  if (x % z == 0) {
+    y.push(z)
+    x /= z
+    return primeFactorsOf(x,y,z)
+  } else if (x > 1) {
+    z++
+    return primeFactorsOf(x,y,z)
+  } else {
+    return y
+  }
+}
+console.log(primeFactorsOf(56))
+
+btnEl.addEventListener("click", function(){
+  let y = primeFactorsOf(Number(inputEl.value))
+  let factors = "["
+  for (let i=0;i<y.length-1;i++){
+    factors += y[i] + ", "
+  }
+  factors += y[y.length-1]+"]"
+  outputEl.innerHTML = factors
+})
